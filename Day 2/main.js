c91b0b55a418cbc9cf1d9e650a02dd4718a039ca
Moises Machado <moises.rm@hotmail.com>
2020-01-03 01:09:52 -0300

    Second day

--- Day 2/main.js
+++ Day 2/main.js
@@ -95,4 +95,4 @@ console.log(totalAnnual)
 
 //Exercise 30
 var crazySentence = "%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?"
-console.log(crazySentence.replace(/[^A-Za-z\s]*/g,''))
+console.log(crazySentence.replace(/[^A-Za-z\s\.\?]*/g,''))
