894cf331437a78b9f8f8cf048472123713bc8760
Moises Machado <moises.rm@hotmail.com>
2020-01-04 19:41:30 -0300

    Updated day 2

--- Day 2/main.js
+++ Day 2/main.js
@@ -113,3 +113,25 @@ console.log("The quote 'There is no exercise better for the heart than reaching 
 
 //Exercise 32
 console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"")
+
+//Exercise 33
+console.log("python".match(/on/))
+console.log("jargon".match(/on/))
+
+//Exercise 34
+console.log("I hope this course is not full of jargon".match(/jargon/))
+
+//Exercise 35
+console.log(Math.floor(Math.random () * 101))
+
+//Exercise 36
+console.log(Math.floor(Math.random () * 51) + 50)
+
+//Exercise 37
+console.log(Math.floor(Math.random () * 256))
+
+//Exercise 38
+console.log('JavaScript'.charAt(Math.random() * 10))
+
+//Exercise 39
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125")
